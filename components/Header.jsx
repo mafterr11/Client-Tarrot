7fc08fe9f183f133117a06ba02f071b98f964bc9
mafterr11 <mafterr11@gmail.com>
2024-06-20 21:26:08 +0300

    .

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,7 +6,7 @@ import { PhoneCallIcon } from "lucide-react";
 
 const Header = () => {
   return (
-    <div className="fixed left-0 right-0 top-0 z-50 w-full border-b-[0.5px] border-black/50 bg-gradient-to-t from-[#91221E] to-[#030202] py-[12px] filter backdrop-blur-md transition-all duration-100 ease-in-out max-md:px-[25px] md:max-xl:px-[40px] xl:px-[55px]">
+    <div className="fixed left-0 right-0 top-0 z-50 w-full border-b-[0.5px] border-black/50 bg-gradient-to-t from-[#91221E]/25 to-[#030202]/25 py-[12px] filter backdrop-blur-md transition-all duration-100 ease-in-out max-md:px-[25px] md:max-xl:px-[40px] xl:px-[55px]">
       <div className="relative z-40 flex items-center justify-between">
         <Logo />
         <div className="flex items-center justify-center">
